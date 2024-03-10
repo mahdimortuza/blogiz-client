189b2dbd361cf051db6ca3518000a135ecd036a9
mahdimortuza <mahdi.webx@gmail.com>
2024-03-10 15:48:58 +0600

    api route created

---
+++ src/app/api/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  return NextResponse.json({ message: "hello from server" });
+};
+
+export const POST = async () => {
+  return NextResponse.json({ message: "hello from server" });
+};
+
+export const DELETE = async () => {
+  return NextResponse.json({ message: "hello from server" });
+};
+
+export const PUT = async () => {
+  return NextResponse.json({ message: "hello from server" });
+};
+
+export const PATH = async () => {
+  return NextResponse.json({ message: "hello from server" });
+};
